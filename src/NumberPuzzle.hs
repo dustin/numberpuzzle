e7460917dcb53983df7f4934a7930eeb0f840c32
Dustin Sallings <dustin@spy.net>
2018-08-25 12:23:37 -0700

    Expression functions can have arbitrary operands.
    Also added rpnify from an expression and tests and stuff.
    
        λ> show_samples
        8/8 -> 8/8 -> [8,8,/]
        5-1 -> 5-1 -> [5,1,-]
        5+2 -> 2+5 -> [2,5,+]
        1+7 -> 1+7 -> [1,7,+]
        3+6 -> 3+6 -> [3,6,+]
        3*4 -> 3*4 -> [3,4,*]
        8*4 -> 4*8 -> [4,8,*]
        8*6 -> 6*8 -> [6,8,*]
        7^2 -> 7^2 -> [7,2,^]
        (6+4)/0 -> (6+4)/0 -> [6,4,+,0,/]
        (4*(4-((2^1)-((3/4)*9))))/(4+2) -> (4*(4-((2^1)-((3/4)*9))))/(4+2) -> [4,4,2,1,^,3,4,/,9,*,-,-,*,4,2,+,/]

--- src/NumberPuzzle.hs
+++ src/NumberPuzzle.hs
@@ -2,17 +2,18 @@ module NumberPuzzle
     ( Value(..),
       Expression(..),
       eval, solve,
-      operators, canonicalize, dedup, exprify, evalexpr, canonicalizeexpr, depth
+      operators, canonicalize, dedup, exprify, evalexpr, canonicalizeexpr, depth, rpnify
     ) where
 
 import qualified Data.Set as Set
-import Data.Semigroup ((<>))
-import Data.Ord (comparing)
-import Data.Function (on)
-import Data.Foldable (minimumBy, maximumBy)
 import Control.Applicative (liftA2)
-import Data.List (sort, permutations);
 import Control.Monad (guard, replicateM);
+import Data.Foldable (minimumBy, maximumBy)
+import Data.Function (on)
+import Data.List (sort, sortBy, permutations, intercalate);
+import Data.Maybe (fromJust)
+import Data.Ord (comparing)
+import Data.Semigroup ((<>))
 
 data Value = Fun (String, Int -> Int -> Maybe Int)
            | Val Int
@@ -50,17 +51,17 @@ operators = map (\(s,f) -> Fun (s, justice f)) [("+", (+)),
         justice f a b = pure $ f a b
 
 data Expression = EVal Int
-                | EFun (String, Int -> Int -> Maybe Int) Expression Expression
+                | EFun (String, Int -> Int -> Maybe Int) [Expression]
 
 instance Show Expression where
   show (EVal x) = show x
-  show (EFun (fn, _) l r) = inner l <> " " <> fn <> " " <> inner r
+  show (EFun (fn, _) exps) = intercalate fn (map inner exps)
     where inner (EVal x) = show x
-          inner (EFun (fn,_) l r) = "(" <> inner l <> " " <> fn <> " " <> inner r <> ")"
+          inner (EFun (fn,_) exps) = "(" <> intercalate fn (map inner exps) <> ")"
 
 depth :: Expression -> Int
 depth (EVal _) = 0
-depth (EFun _ l r) = 1 + depth l + depth r
+depth (EFun _ exps) = 1 + foldr (\x o -> o + depth x) 0 exps
 
 instance Eq Expression where (==) = on (==) show
 
@@ -74,32 +75,49 @@ exprify = go []
   where go :: [Expression] -> [Value] -> Maybe Expression
         go [x] [] = pure x
         go st (Val v:ops) = go (EVal v:st) ops
-        go (v1:v2:st) (Fun f:ops) = go (EFun f v2 v1:st) ops
+        go (v1:v2:st) (Fun f:ops) = go (EFun f [v2,v1]:st) ops
         go _ _ = Nothing
 
 evalexpr :: Expression -> Maybe Int
 evalexpr (EVal v) = pure v
-evalexpr (EFun (_,f) l r) = do
-  l' <- evalexpr l
-  r' <- evalexpr r
-  f l' r'
+evalexpr (EFun (_,f) exps) = foldr1 (lft f) (map evalexpr exps)
+
+  where lft :: Monad m => (a -> a -> m a) -> (m a -> m a -> m a)
+        lft f a b = do
+          a' <- a
+          b' <- b
+          f a' b'
+
+rpnify :: Expression -> [Value]
+rpnify (EVal x) = [Val x]
+rpnify (EFun f exprs) = concatMap rpnify exprs <> [Fun f]
 
 canonicalizeexpr :: Expression -> Expression
-canonicalizeexpr e@(EFun _ _ _) = commute e
+canonicalizeexpr e@(EFun _ _) = (commute . associate) e
   where
-    commute e@(EFun f@(fn,_) l r)
-      | commutes fn = EFun f (canonicalizeexpr $ mn l r) (canonicalizeexpr $ mx l r)
+    commute :: Expression -> Expression
+    commute e@(EFun f@(fn,_) exps)
+      | commutes fn = EFun f (map canonicalizeexpr $ sortBy (comparing evalexpr) exps)
       | otherwise = e
-
-    mn :: Expression -> Expression -> Expression
-    mn a b = minimumBy (on compare evalexpr) [a, b]
-
-    mx :: Expression -> Expression -> Expression
-    mx a b = maximumBy (on compare evalexpr) [a, b]
 
     commutes "*" = True
     commutes "+" = True
     commutes _ = False
+
+    associate :: Expression -> Expression
+    -- (1+2)+3 -> 1+2+3
+    associate e@(EFun f@(fn,_) ((EFun (fn',_) inner):rest))
+      | associates fn fn' = EFun f (inner <> rest)
+      | otherwise = e
+      -- 1+(2+3) -> 1+2+3
+    associate e@(EFun f@(fn,_) (l:(EFun (fn',_) inner):rest))
+      | associates fn fn' = EFun f (l:inner <> rest)
+      | otherwise = e
+    associate x = x
+
+    associates "*" "*" = True
+    associates "+" "+" = True
+    associates _ _ = False
 canonicalizeexpr x = x
 
 canonicalize :: [Value] -> [Value]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,9 @@
 import NumberPuzzle
 
+import Control.Monad (forM_)
 import Data.Semigroup ((<>))
 import Data.Maybe (isJust)
-import Data.List (nub)
+import Data.List (nub, sort)
 
 import Test.Tasty
 import Test.QuickCheck
@@ -29,16 +30,26 @@ instance Arbitrary Expression where
     op <- oneof $ map (\(NumberPuzzle.Fun x) -> pure (EFun x)) operators
     s1 <- subex
     s2 <- subex
-    pure $ op s1 s2
+    pure $ op [s1, s2]
 
     where
       subex :: Gen Expression
       subex = do
         frequency [
-          (4, EVal <$> choose (0,9)),
+          (3, EVal <$> choose (0,9)),
           (1, arbitrary)
           ]
 
+show_samples :: IO ()
+show_samples = do
+  samples <- sample' (arbitrary :: Gen Expression)
+  forM_ (sort samples) (\s -> do
+                           (putStr.show) s
+                           putStr " -> "
+                           (putStr.show.canonicalizeexpr) s
+                           putStr " -> "
+                           (print.rpnify.canonicalizeexpr) s
+                       )
 
 prop_dedup :: Values -> Bool
 prop_dedup (Values v) = nub v == dedup v
@@ -56,13 +67,17 @@ prop_exprcanon = evalexpr <=> evalexpr.canonicalizeexpr
 prop_newform :: Values -> Bool
 prop_newform (Values v) = (evalexpr =<< exprify v) == eval v
 
+prop_rpnform :: Expression -> Bool
+prop_rpnform = evalexpr <=> eval.rpnify
+
 tests :: [TestTree]
 tests = [
   testProperty "canonicalizes" prop_canon,
   testProperty "dedups [Value]" prop_dedup,
   testProperty "dedups [[Value]]" prop_dedup2,
   testProperty "new form works" prop_newform,
-  testProperty "expr canon" prop_exprcanon
+  testProperty "expr canon" prop_exprcanon,
+  testProperty "rpn expr" prop_rpnform
   ]
 
 main :: IO ()
